2376d204b3e558a7f39020aa477fb86aacd933aa
Salman <144887308+salmandevel0per@users.noreply.github.com>
2023-10-18 13:28:25 +0000

    datatypes

---
+++ .devcontainer/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 14453
+let accountEmail = "sallu.windows@gmail.com"
+var accountPassword = "12345"
+accountCity = "Mandapeta"
+let accountState;
+
+// accountId = 2 // not allowed
+accountEmail = "windows@gmail.com"
+accountPassword = "01234"
+accountCity = "modukuru"
+console.log(typeof accountId);
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
---
+++ .devcontainer/02_datatypes.js
@@ -0,0 +1,9 @@
+"use strict";
+
+
+console.log(3 + 3);
+
+console.log("Salman");
+
+console.log(typeof "salman");
+console.log(typeof null);
